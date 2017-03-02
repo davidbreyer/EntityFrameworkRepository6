20b0ba1fd5f8a05a82c760b06d2d6b1edb301425
David Breyer <davidbreyer@gmail.com>
2017-03-02 09:18:09 -0500

    Code Cleanup in Audit Base Context

--- EntityFrameworkAuditableRepository6/AuditableBaseContext.cs
+++ EntityFrameworkAuditableRepository6/AuditableBaseContext.cs
@@ -7,30 +7,18 @@ namespace EntityFramework.Auditable.Repository6
     public class AuditableBaseContext<TContext> : AuditDbContext
             where TContext : AuditDbContext
     {
-        static AuditableBaseContext()
-        {
-            //Database.SetInitializer<TContext>(null);
-        }
-
+        static AuditableBaseContext() { }
+    
         protected AuditableBaseContext(DbConnection dbConnection) : base(dbConnection, true) { Database.SetInitializer<TContext>(null); }
 
         protected AuditableBaseContext(DbConnection dbConnection, bool contextOwnsConnection) : base(dbConnection, contextOwnsConnection) { Database.SetInitializer<TContext>(null); }
 
         protected AuditableBaseContext(string connectionString) : base(connectionString) { Database.SetInitializer<TContext>(null); }
 
-        protected AuditableBaseContext(string connectionString, IDatabaseInitializer<TContext> initializerStrategy) : base(connectionString)
-        {
-            Database.SetInitializer<TContext>(initializerStrategy);
-        }
+        protected AuditableBaseContext(string connectionString, IDatabaseInitializer<TContext> initializerStrategy) : base(connectionString) { Database.SetInitializer<TContext>(initializerStrategy); }
 
-        protected AuditableBaseContext(DbConnection dbConnection, IDatabaseInitializer<TContext> initializerStrategy) : base(dbConnection, true)
-        {
-            Database.SetInitializer<TContext>(initializerStrategy);
-        }
+        protected AuditableBaseContext(DbConnection dbConnection, IDatabaseInitializer<TContext> initializerStrategy) : base(dbConnection, true) { Database.SetInitializer<TContext>(initializerStrategy); }
 
-        protected AuditableBaseContext(DbConnection dbConnection, IDatabaseInitializer<TContext> initializerStrategy, bool contextOwnsConnection) : base(dbConnection, contextOwnsConnection)
-        {
-            Database.SetInitializer<TContext>(initializerStrategy);
-        }
+        protected AuditableBaseContext(DbConnection dbConnection, IDatabaseInitializer<TContext> initializerStrategy, bool contextOwnsConnection) : base(dbConnection, contextOwnsConnection) { Database.SetInitializer<TContext>(initializerStrategy); }
     }
 }
